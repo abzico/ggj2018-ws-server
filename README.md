14b9574650aff5719cfd814890fd3105d060fe8b
Wasin Thonkaew <haxpor@gmail.com>
2018-01-26 10:18:31 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# ggj2018-ws-server
+
+Websocket server for to-be-made GGJ2018 game.
+
+# License
+
+Under [Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License](https://github.com/haxpor/fkit/blob/master/LICENSE).
---
+++ index.js
@@ -0,0 +1,22 @@
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({ port: 6666 });
+
+wss.on('connection', function(ws, req) {
+	console.log('new connection from client ip[' + req.headers['x-forwarded-for'] + ']');
+
+	ws.on('message', function(message) {
+		console.log('received message: ' + message);
+
+		ws.on('error', (e) => { console.log(e); });
+		
+		try {
+			ws.send(message);	
+		}
+		catch(e) {
+			console.log('error: ', e);
+		}
+	});
+});
+
+console.log('server started');
